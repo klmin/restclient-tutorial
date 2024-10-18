54a9bcb066060d6164ee775e8ac1bc15b2ef9e13
klmin <haemmas@naver.com>
2024-10-19 02:27:51 +0900

    feat(webclient): Add ObjectMapper

--- src/main/java/com/restclient/webclient/config/WebClientConfig.java
+++ src/main/java/com/restclient/webclient/config/WebClientConfig.java
@@ -1,15 +1,23 @@
 package com.restclient.webclient.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
-    public WebClient webClient() {
-        return WebClient.create();
+    public WebClient webClient(ObjectMapper objectMapper) {
+        return WebClient.builder()
+                .codecs(configurer -> {
+                    configurer.defaultCodecs().jackson2JsonEncoder(new Jackson2JsonEncoder(objectMapper));
+                    configurer.defaultCodecs().jackson2JsonDecoder(new Jackson2JsonDecoder(objectMapper));
+                })
+                    .build();
 
     }
 
