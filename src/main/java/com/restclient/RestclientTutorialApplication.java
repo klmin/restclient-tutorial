6f4fb515bcf188c26e268fc38f11f414303f69a3
klmin <haemmas@naver.com>
2024-10-18 19:40:34 +0900

    init

---
+++ src/main/java/com/restclient/RestclientTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.restclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestclientTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestclientTutorialApplication.class, args);
+    }
+
+}
