0e7d83a8fb22a27f70180962e5fb0c28648c41a0
klmin <haemmas@naver.com>
2024-10-20 22:58:27 +0900

    feat(resttemplate): Update RestTemplateConfig

--- src/main/java/com/restclient/resttemplate/config/RestTemplateConfig.java
+++ src/main/java/com/restclient/resttemplate/config/RestTemplateConfig.java
@@ -29,43 +29,51 @@ import java.util.List;
 @Configuration
 public class RestTemplateConfig {
 
+    @Bean
     public HttpClient httpClient(){
 
-        int maxPool = 200; // 전체 커넥션 풀
-        int maxPerRoute = 100; // 호스트 당 커넥션 풀에서 쓸 수 있는 커넥션 개수
-        int idleConnectionTimeoutSec = 30; // 유휴 커넥션이 이 시간이 지나면 커넥션 반납
-        long requestTimeOut = 5000; // 커넥션 풀로부터 커넥션 받기를 대기하는 시간
-        long readTimeOut = 5000; // 서버로부터 응답을 받기 위한 최대 대기 시간
-        int retryCount = 1; // 어떠한 예외로 인해 처리하지 못한 요청에 대한 retry 횟수
-        long backoff = 1000; // retry 간 back off
+        int maxPool = 200; // 전체 커넥션 풀에서 사용할 수 있는 최대 커넥션 수
+        int maxPerRoute = 100; // 각 호스트(서버)당 커넥션 풀에서 사용할 수 있는 최대 커넥션 수
+        long validateAfterInactivity = 5L; // 유휴상태 검증 시간
+        int idleConnectionTimeoutSec = 30; // 유휴 커넥션이 이 시간이 지나면 풀에서 제거되는 시간
+        long requestTimeOut = 5000L; // 커넥션 풀에서 커넥션을 얻기 위해 대기할 수 있는 최대 시간
+        long readTimeOut = 5000L; // 서버로부터 응답을 기다리는 최대 시간
+        int retryCount = 1; // 예외가 발생했을 때 요청을 재시도할 최대 횟수
+        long backoff = 1000; // 재시도 간의 대기 시간
 
+        // 커넥션 관리 매니저 (커넥션 풀을 관리하는 매니저)
         PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager();
         connManager.setMaxTotal(maxPool);
         connManager.setDefaultMaxPerRoute(maxPerRoute);
-        connManager.setDefaultSocketConfig(SocketConfig.DEFAULT);
-        connManager.setDefaultConnectionConfig(ConnectionConfig.custom().setValidateAfterInactivity(Timeout.ofSeconds(5)).build());
+        connManager.setDefaultSocketConfig(SocketConfig.DEFAULT); // 기본 TCP 소켓 설정
+        connManager.setDefaultConnectionConfig(ConnectionConfig
+                .custom()
+                .setValidateAfterInactivity(Timeout.ofSeconds(validateAfterInactivity))
+                .build()); // 유휴 상태에서 커넥션을 재사용하기 전에 유효성 검사를 수행 (서버와의 연결이 끊겼는지 확인)
 
+
+        // 요청 관련 다양한 설정을 제공 (타임아웃 관련 설정)
         RequestConfig requestConfig = RequestConfig.custom()
-                .setConnectionRequestTimeout(Timeout.ofMilliseconds(requestTimeOut)) // 연결 타임아웃
-                .setResponseTimeout(Timeout.ofMilliseconds(readTimeOut)) // 읽기 타임아웃
+                .setConnectionRequestTimeout(Timeout.ofMilliseconds(requestTimeOut))
+                .setResponseTimeout(Timeout.ofMilliseconds(readTimeOut))
                 .build();
 
         return HttpClients.custom()
                 .setConnectionManager(connManager)
                 .setDefaultRequestConfig(requestConfig)
-                .setConnectionBackoffStrategy(new DefaultBackoffStrategy()) //어떤 상황에서 백오프를 할지(위 옵션과 매칭되는 옵션)
-                .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy()) //default 값 서버에서 받은 keep-alive
-                .setRetryStrategy(new DefaultHttpRequestRetryStrategy(retryCount, TimeValue.ofMilliseconds(backoff))) // 몇번 재시도 할지
-                .evictIdleConnections(TimeValue.ofSeconds(idleConnectionTimeoutSec)) // 유휴 커넥션이 이 시간(초)동안 존재하면 커넥션 종료
+                .setConnectionBackoffStrategy(new DefaultBackoffStrategy()) // 네트워크 오류 발생 시 재시도 간 백오프전략
+                .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy()) // 서버가 보낸 Keep-Alive 헤더 값에 따라 커넥션을 유지하는 전략
+                .setRetryStrategy(new DefaultHttpRequestRetryStrategy(retryCount, TimeValue.ofMilliseconds(backoff))) // 네트워크 오류 또는 일시적인 서버 문제 발생 시 재시도 전략
+                .evictIdleConnections(TimeValue.ofSeconds(idleConnectionTimeoutSec)) // 유휴 커넥션이 사용되지 않았을 경우 커넥션을 종료하고 풀에서 제거
                 .build();
     }
 
     @Bean
-    public RestTemplate restTemplate(ObjectMapper objectMapper){
+    public RestTemplate restTemplate(ObjectMapper objectMapper, HttpClient httpClient){
 
         RestTemplate restTemplate = new RestTemplate();
 
-        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient());
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
 
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
         converter.setObjectMapper(objectMapper);
