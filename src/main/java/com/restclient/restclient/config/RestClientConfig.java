08c2a98e4fec3b923a0ddff779e087026c35de09
klmin <haemmas@naver.com>
2024-10-20 23:04:32 +0900

    refactor(httpclient): Extract HttpClient configuration to separate file

--- src/main/java/com/restclient/restclient/config/RestClientConfig.java
+++ src/main/java/com/restclient/restclient/config/RestClientConfig.java
@@ -1,9 +1,10 @@
 package com.restclient.restclient.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.restclient.resttemplate.interceptor.RestRequestInterceptor;
+import org.apache.hc.client5.http.classic.HttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestClient;
 
@@ -11,13 +12,14 @@ import org.springframework.web.client.RestClient;
 public class RestClientConfig {
 
     @Bean
-    public RestClient restClient(ObjectMapper objectMapper){
+    public RestClient restClient(ObjectMapper objectMapper, HttpClient httpClient) {
         return RestClient.builder()
                 .messageConverters(
                         converters -> {
                             converters.removeIf(MappingJackson2HttpMessageConverter.class::isInstance);
                             converters.add(new MappingJackson2HttpMessageConverter(objectMapper));
                         })
+                .requestFactory(new HttpComponentsClientHttpRequestFactory(httpClient))
                 .build();
     }
 }
